a321f1f3260fc71e2de598f6fa1e0a03042cc4f7
unknown <thomas.zammit@MTDKP116.ix.com>
2014-07-11 16:14:32 +0200

    Minor #hashtag bug fix

--- post.js
+++ post.js
@@ -3,7 +3,7 @@ var button = document.getElementById("tweetButton");
 button.onclick = function() { 
 	$.post("http://localhost:8080/twitterlite-ws/tweets?username="+
 		document.getElementById("usernameInput").value+
-		"&content="+document.getElementById("tweetInput").value, function( data ) {
+		"&content="+document.getElementById("tweetInput").value.replace(/#/g,"%23"), function( data ) {
 			console.log(document.getElementById("usernameInput").value + " " + document.getElementById("tweetInput").value);
 		});
 }
