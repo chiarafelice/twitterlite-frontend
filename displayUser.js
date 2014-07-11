77047840ad072dc55d9cb070cde7f29bd16ae903
unknown <thomas.zammit@MTDKP116.ix.com>
2014-07-11 13:55:58 +0200

    Getting from db and displaying

--- displayUser.js
+++ displayUser.js
@@ -4,8 +4,37 @@ var username = usernameQS.split("=");
 
 if(username[1]!=null){
 $.get( "http://localhost:8080/twitterlite-ws/messages/user?user="+username[1], function( data ) {
-  $( ".result" ).html( data );
  console.log(data);
+	for (i = 0; i < data.length; i++) { 
+	//	console.log();
+		//console.log();
+		
+		var parentDiv = document.getElementById("content");
+		
+		var container = document.createElement("div");
+		container.setAttribute("class","list-group");
+		
+		var containerSon = document.createElement("div");
+		containerSon.setAttribute("class","list-group-item");
+		
+		var p=  document.createElement("p");
+		p.setAttribute("class","list-group-item-text");
+		p.innerHTML="@"+data[i].username;
+		
+		var h4 = document.createElement("h4");
+		h4.setAttribute("class","list-group-item-heading");
+		h4.innerHTML=data[i].content;
+		
+
+		containerSon.appendChild(p);
+		containerSon.appendChild(h4);
+		
+		container.appendChild(containerSon);
+
+		parentDiv.appendChild(container);
+		
+		console.log(container);
+	}
 });
 }else{
 window.location="error.html";
