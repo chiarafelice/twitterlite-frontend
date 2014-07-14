c858f3bbcf0501890e099527246ef69f0162eca7
unknown <thomas.zammit@MTDKP116.ix.com>
2014-07-14 10:11:09 +0200

    zidtilek ir-refresh life

--- post.js
+++ post.js
@@ -4,7 +4,7 @@ button.onclick = function() {
 	$.post("http://localhost:8080/twitterlite-ws/tweets?username="+
 		document.getElementById("usernameInput").value+
 		"&content="+document.getElementById("tweetInput").value.replace(/#/g,"%23"), function( data ) {
-			console.log(document.getElementById("usernameInput").value + " " + document.getElementById("tweetInput").value);
+			location.reload();
 		});
 }
 
