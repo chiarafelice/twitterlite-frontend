76dd8f9fe2c981163365d322456441f0d832e8d2
unknown <thomas.zammit@MTDKP116.ix.com>
2014-07-14 09:27:20 +0200

    Fixed project structure, hashtag page working. Use /content.html?user= or /content.html?hashtags=

--- displayQueryDb.js
+++ displayQueryDb.js
@@ -1,13 +1,20 @@
 var loca = location.search;
-var usernameQS = loca.replace(/%22/g,"");
-var username = usernameQS.split("=");
+var queryString = loca.replace(/%22/g,"");
+var query = queryString.split("=");
+var url = "";
 
-if(username[1]!=null){
-$.get( "http://localhost:8080/twitterlite-ws/messages/user?user="+username[1]"offset=0&limit=10", function( data ) {
+if(query[1]!=null){
+if(query[0]=="?user"){
+url = "http://localhost:8080/twitterlite-ws/messages/user?user="+query[1]+"&offset=0&limit=10";
+}else if(query[0]=="?hashtag"){
+url = "http://localhost:8080/twitterlite-ws/messages/hashtags?hashtags="+query[1]+"&offset=0&limit=10";
+}else{
+window.location="error.html";
+}
+$.get(url, function( data ) {
  console.log(data);
 	for (i = 0; i < data.length; i++) { 
 	//	console.log();
-		//console.log();
 		
 		var parentDiv = document.getElementById("content");
 		
@@ -17,7 +24,7 @@ $.get( "http://localhost:8080/twitterlite-ws/messages/user?user="+username[1]"of
 		var containerSon = document.createElement("div");
 		containerSon.setAttribute("class","list-group-item");
 		
-		var p=  document.createElement("p");
+		var p=document.createElement("p");
 		p.setAttribute("class","list-group-item-text");
 		p.innerHTML="@"+data[i].username;
 		
