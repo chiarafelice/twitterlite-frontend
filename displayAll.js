64815b6408702fe7d31a492f22f2639e67886504
Chiara Felice Sant Cassia <chiara.felice@ixaris.com>
2014-07-14 11:16:24 +0200

    set mentions and hashtags

--- displayAll.js
+++ displayAll.js
@@ -11,12 +11,44 @@ $.get( "http://localhost:8080/twitterlite-ws/messages?offset=0&limit=22", functi
 		
 		var p=  document.createElement("p");
 		p.setAttribute("class","list-group-item-text");
-		p.innerHTML="@"+data[i].username;
+
+    	var url = "content.html?user=%22"+data[i].username +"%22";
+
+    	p.innerHTML = "<a href=" + url + ">" + "@"+ data[i].username + " </a>";
+
 		
 		var h4 = document.createElement("h4");
 		h4.setAttribute("class","list-group-item-heading");
-		h4.innerHTML=data[i].content;
-		
+
+
+		var tagslistarr = data[i].content.split(' ');
+		var arr=[];
+
+		var tweet = "";
+
+		$.each(tagslistarr,function(i,val){
+		    if(tagslistarr[i].indexOf('#') == 0){
+		    	var tag = tagslistarr[i].substring(1);
+		    	var url = "content.html?hashtag=%22"+tag+"%22";
+		    	
+		    	tweet += "<a href=" + url + ">" + tagslistarr[i] + " </a>";
+		        
+		    } else if(tagslistarr[i].indexOf('@') == 0){
+		    	var user = tagslistarr[i].substring(1);
+
+		    	var url = "content.html?user=%22"+user+"%22";
+		    	tweet += "<a href=" + url + ">" + tagslistarr[i] + " </a>";
+
+		    } else {
+		    	tweet += tagslistarr[i] + " ";
+		    }
+		});
+
+
+
+
+		// h4.innerHTML=data[i].content;
+		h4.innerHTML = tweet;
 
 		containerSon.appendChild(p);
 		containerSon.appendChild(h4);
